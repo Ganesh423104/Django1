dc7b071655ca410143aeae25244d34b1105fe40f
Ganesh Kekan <ganeshkekan2021@gmail.com>
2024-03-17 20:31:33 +0530

    Initial commit

---
+++ e_comm/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Sub_Category, Product, Contact_us, Order, Books
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Sub_Category)
+admin.site.register(Product)
+admin.site.register(Contact_us)
+admin.site.register(Order)
+admin.site.register(Books)
+
